90c77fe8866ef7c0670dccddaf4b36636329c8bb
VladimirDuisenbinov <vladimir.duisenbinov@nu.edu.kz>
2019-05-08 22:28:26 +0600

    Commit

---
+++ static/precache-manifest.3e187d7cfd2244bd7a3f35f8b0d706e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5899a904d8f5f10e7f9b",
+    "url": "/static/js/main.988827ce.chunk.js"
+  },
+  {
+    "revision": "4ca19de2d5f4a41cc5d9",
+    "url": "/static/js/2.c98f0356.chunk.js"
+  },
+  {
+    "revision": "5899a904d8f5f10e7f9b",
+    "url": "/static/css/main.6c3a5690.chunk.css"
+  },
+  {
+    "revision": "4ca19de2d5f4a41cc5d9",
+    "url": "/static/css/2.9f4f2c96.chunk.css"
+  },
+  {
+    "revision": "ebaf94febbe202f3799f48ec18ef71a2",
+    "url": "/index.html"
+  }
+];
